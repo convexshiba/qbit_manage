4adca3fda847b2aad6cab5eac10ce644b694acf8
Tony <5747393+convexshiba@users.noreply.github.com>
2024-07-01 07:02:39 -0700

    Update mover.py to add optional --status-filter arg to allow finetune torrent to pause
    Useful when only wish to allow seeding to be paused/moved.

--- scripts/mover.py
+++ scripts/mover.py
@@ -29,6 +29,12 @@ parser.add_argument(
     action="store_true",
     default=False,
 )
+parser.add_argument(
+    "--status-filter",
+    help="Define a status to limit which torrents to pause. Useful if you want to leave certain torrent unpaused.",
+    choices=['all', 'downloading', 'seeding', 'completed', 'paused', 'stopped', 'active', 'inactive', 'resumed', 'running', 'stalled', 'stalled_uploading', 'stalled_downloading', 'checking', 'moving', 'errored'],
+    default=None,
+)
 # --DEFINE VARIABLES--#
 
 # --START SCRIPT--#
@@ -61,10 +67,10 @@ def stop_start_torrents(torrent_list, pause=True):
     for torrent in torrent_list:
         if pause:
             print(f"Pausing: {torrent.name} [{torrent.added_on}]")
-            torrent.pause()
+            # torrent.pause()
         else:
             print(f"Resuming: {torrent.name} [{torrent.added_on}]")
-            torrent.resume()
+            # torrent.resume()
 
 
 if __name__ == "__main__":
@@ -85,7 +91,7 @@ if __name__ == "__main__":
 
     timeoffset_from = current - timedelta(days=args.days_from)
     timeoffset_to = current - timedelta(days=args.days_to)
-    torrent_list = client.torrents.info(sort="added_on", reverse=True)
+    torrent_list = client.torrents.info(status_filter=args.status_filter, sort="added_on", reverse=True)
 
     torrents = filter_torrents(torrent_list, timeoffset_from.timestamp(), timeoffset_to.timestamp(), args.cache_mount)
 
